00b7846aabfe2d0ca5014829bd222a9207dd94bc
megumii <contact@megumii.xyz>
2023-11-15 16:48:13 +0700

    Update run.sh

--- run.sh
+++ run.sh
@@ -2,7 +2,7 @@
 
 while true; do
     response=$(curl -k -f https://www.pilotdog.tech/api/get_eligible_proof?address=ADDRESSMU 2>&1)
-    if [[ $response == *"502 Bad Gateway"* ]]; then
+    if [[ $response == *"502 Bad Gateway Nginx Error"* ]]; then
         echo "Received Bad Gateway error. Retrying in 60 seconds..."
         sleep 60
     else
