b5c2ba78dd2e42c24523a38769de03e18c46da4c
megumii <contact@megumii.xyz>
2023-11-15 17:41:45 +0700

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,14 +1,10 @@
 #!/bin/bash
 
-while true; do
-    response=$(curl -k -f https://www.pilotdog.tech/api/get_eligible_proof?address=ADDRESSMU 2>&1)
-    if [[ $response == *"502 Bad Gateway Nginx Error"* ]]; then
-        echo "Received Bad Gateway error. Retrying in 60 seconds..."
-        sleep 60
-    else
-        echo "Request successful."
-        # Do something with the response (e.g., update the file)
-        echo "$response" >> Update.tsx
-        break
-    fi
-done
+# Menjalankan perintah curl dan menyimpan outputnya ke dalam file Update.tsx
+curl_output=$(curl -k -f https://www.pilotdog.tech/api/get_eligible_proof?address=ADDRESSMU 2>&1)
+
+# Menyimpan hasil output curl ke dalam file Update.tsx
+echo "$curl_output" >> Update.tsx
+
+# Menambahkan pesan bahwa Update.tsx telah diperbarui
+echo "Check Update.tsx." 
