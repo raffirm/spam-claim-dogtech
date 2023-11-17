bc5ff1482b92d89f991f7caf00aa1e75388d29fa
raffirm <mursalimrafi008@gmail.com>
2023-11-17 00:31:55 +0000

    🟤 auto commit

--- Update.tsx
+++ Update.tsx
@@ -26463,3 +26463,7 @@ curl: (22) The requested URL returned error: 502
                                  Dload  Upload   Total   Spent    Left  Speed
   0     0    0     0    0     0      0      0 --:--:-- --:--:-- --:--:--     0100    60  100    60    0     0    679      0 --:--:-- --:--:-- --:--:--   689
 {"code":"InvalidParameters","error":"not found","data":null}
+  % Total    % Received % Xferd  Average Speed   Time    Time     Time  Current
+                                 Dload  Upload   Total   Spent    Left  Speed
+  0     0    0     0    0     0      0      0 --:--:-- --:--:-- --:--:--     0100    60  100    60    0     0    224      0 --:--:-- --:--:-- --:--:--   225
+{"code":"InvalidParameters","error":"not found","data":null}
